7a04f3e77fc6088bb4c778fde5535e7e69dd284a
Amauri Dall'Oglio <amauridalloglio@outlook.com.br>
2024-11-10 11:05:24 -0300

    Adicionar arquivos de projeto.

---
+++ ServicoDisparo.Console/HangFire/AgendarUsuarioServico.cs
@@ -0,0 +1,76 @@
+﻿using System.Net.Http.Headers;
+using System.Text;
+
+namespace ServicoDisparo.Console.HangFire
+{
+    public class AgendarUsuarioServico
+    {
+
+        public async Task<string> ValidaConexao(HttpClient httpClient)
+        {
+            string apiUrl = "https://localhost:7250/api/Usuario/Conexao";
+            HttpResponseMessage response = httpClient.GetAsync(apiUrl).Result;
+            var content = response.Content.ReadAsStringAsync().Result;
+            return content;
+        }
+
+
+        public async Task<string> ExecutarAgendarInsercaoSemDados(HttpClient httpClient)
+        {
+            global::System.Console.WriteLine("-----------------------------------------------------");
+            global::System.Console.WriteLine("Iniciando chamada para API: Hangfire/AgendarInsercaoSemDados ");
+            global::System.Console.WriteLine("-----------------------------------------------------");
+
+            try
+            {
+                // Configurar o handler para aceitar qualquer certificado SSL
+                var handler = new HttpClientHandler
+                {
+                    // Ignorar erros de certificado SSL no ambiente de desenvolvimento
+                    ServerCertificateCustomValidationCallback = (sender, cert, chain, sslPolicyErrors) => true
+                };
+
+                using (var client = new HttpClient(handler))
+                {
+          
+                    string apiUrl = "https://127.0.0.1:7250/api/Usuario/AgendarInsercaoSemDados";
+                    client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("*/*"));
+           
+
+
+                    global::System.Console.WriteLine("Executa a requisição POST com corpo vazio para " + apiUrl);
+  
+                    var response = await client.PostAsync(apiUrl, new StringContent("", Encoding.UTF8, "application/json"));
+             
+                    // Lê o conteúdo da resposta
+                    var content = await response.Content.ReadAsStringAsync();
+
+                    // Verifica se a resposta foi bem-sucedida
+                    if (response.IsSuccessStatusCode)
+                    {
+                        global::System.Console.WriteLine("Resposta do servidor:");
+                        global::System.Console.WriteLine(content);
+                    }
+                    else
+                    {
+                        global::System.Console.WriteLine($"Erro: {response.StatusCode}");
+                        global::System.Console.WriteLine($"Detalhes do erro: {content}");
+                    }
+
+                    return content;
+                }
+            }
+            catch (HttpRequestException httpEx)
+            {
+                global::System.Console.WriteLine($"Erro de requisição HTTP: {httpEx.Message}");
+            }
+            catch (Exception ex)
+            {
+                global::System.Console.WriteLine($"Erro geral: {ex.Message}");
+            }
+
+            global::System.Console.WriteLine("Finalizando execução do método Executar.");
+            return string.Empty;
+        }
+    }
+}
---
+++ ServicoDisparo.Console/Program.cs
@@ -0,0 +1,58 @@
+﻿using ServicoDisparo.Console.HangFire;
+using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace ServicoDisparo.Console
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+
+            using (HttpClient httpClient = new HttpClient())
+            {
+                global::System.Console.WriteLine("-----------------------------------------------------");
+                global::System.Console.WriteLine("Testando Api do agendandamento do usuário serviço");
+                global::System.Console.WriteLine("-----------------------------------------------------");
+                AgendarUsuarioServico agendarUsuarioServico = new AgendarUsuarioServico();
+                var resultado = agendarUsuarioServico.ValidaConexao(httpClient);
+                switch (resultado.Result)
+                {
+                    case "Ok":
+                        global::System.Console.WriteLine("Conexao: " + resultado.Result);
+                        break;
+                    default:
+                        global::System.Console.WriteLine("Conexao: " + resultado.Result);
+                        break;
+                }
+
+
+                global::System.Console.WriteLine("-----------------------------------------------------");
+                global::System.Console.WriteLine("Executando o agendandamento do usuário serviço");
+                global::System.Console.WriteLine("-----------------------------------------------------");
+
+                for (int i = 0; i < 10; i++)
+                {
+
+
+                    var resultadoAgendamento = agendarUsuarioServico.ExecutarAgendarInsercaoSemDados(httpClient);
+                    switch (resultado.Result)
+                    {
+                        case "Ok":
+                            global::System.Console.WriteLine("Conexao: " + resultadoAgendamento.Result);
+                            break;
+                        default:
+                            global::System.Console.WriteLine("Conexao: " + resultadoAgendamento.Result);
+                            break;
+                    }
+
+                    await Task.Delay(30000); // 30 segundos = 30000 milissegundos
+                }
+
+            }
+
+
+        }
+    }
+}
